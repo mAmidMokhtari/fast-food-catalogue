4c41c348fb2a4caec62e52c8c1ae588d5b049338
mAmidMokhtari <m.amid.mokhtari@gmail.com>
2023-08-29 00:19:52 +0330

    TR-3 : FastFoodList - implement cmp + style - implement item cmp + style - connecting to server and get data using axios - implement filter Items

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,45 @@
 import "./App.css";
 
+import { useEffect, useState } from "react";
+
+import axios from "./axios";
 import CategoryList from "./CategoryList/categoryList";
+import FastFoodList from "./FastFoodList/fastFoodList";
 import Header from "./Header/header";
+import Loading from "./Loading/loading";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [fastFoodItems, setFastFoodItems] = useState([]);
+
+  const fetchData = async (categoryId = null) => {
+    setLoading(true);
+    const response = await axios.get(
+      `/FastFood/list/${categoryId ? "?categoryId=" + categoryId : ""}`
+    );
+    setLoading(false);
+    setFastFoodItems(response.data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const filterItems = (categoryId) => {
+    fetchData(categoryId);
+  };
+
+  const renderContent = () => {
+    if (loading) {
+      return <Loading theme="dark" />;
+    }
+    return <FastFoodList fastFoodItems={fastFoodItems} />;
+  };
   return (
     <div className="wrapper bg-faded-dark">
       <Header></Header>
-      <CategoryList></CategoryList>
+      <CategoryList filterItems={filterItems}></CategoryList>
+      <div className="container mt-4">{renderContent()}</div>
     </div>
   );
 }
