30c523b4612e24c5cfd65dd3e85b84e9185d18d5
mAmidMokhtari <m.amid.mokhtari@gmail.com>
2023-08-29 14:27:09 +0330

    TR-4 : SearchBar - implement cmp + style

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import "./App.css";
 
 import { useEffect, useState } from "react";
 
+import notFound from "./assets/images/404.png";
 import axios from "./axios";
 import CategoryList from "./CategoryList/categoryList";
 import FastFoodList from "./FastFoodList/fastFoodList";
 import Header from "./Header/header";
 import Loading from "./Loading/loading";
+import SearchBar from "./SearchBar/searchBar";
 
 function App() {
   const [loading, setLoading] = useState(false);
@@ -29,16 +31,37 @@ function App() {
     fetchData(categoryId);
   };
 
+  const searchItems = async (term) => {
+    setLoading(true);
+    const response = await axios.get(
+      `/FastFood/search/${term ? "?term=" + term : ""}`
+    );
+    setLoading(false);
+    setFastFoodItems(response.data);
+  };
+
   const renderContent = () => {
     if (loading) {
       return <Loading theme="dark" />;
+    }
+    if (fastFoodItems.length === 0) {
+      return (
+        <>
+          <div className="alert alert-warning text-center">
+            برای کلید واژه فوق هیچ آیتمی یافت نشد
+          </div>
+          <img className="mx-auto mt-5 d-block" src={notFound} />
+        </>
+      );
     }
     return <FastFoodList fastFoodItems={fastFoodItems} />;
   };
   return (
     <div className="wrapper bg-faded-dark">
       <Header></Header>
-      <CategoryList filterItems={filterItems}></CategoryList>
+      <CategoryList filterItems={filterItems}>
+        <SearchBar searchItems={searchItems} />
+      </CategoryList>
       <div className="container mt-4">{renderContent()}</div>
     </div>
   );
--- src/CategoryList/categoryList.jsx
+++ src/CategoryList/categoryList.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import axios from "../axios";
 import Loading from "../Loading/loading";
 
-const CategoryList = ({ filterItems }) => {
+const CategoryList = ({ filterItems, children }) => {
   const [loading, setLoading] = useState(true);
   const [categories, setCategories] = useState([]);
 
@@ -21,24 +21,27 @@ const CategoryList = ({ filterItems }) => {
       return <Loading theme="primary" />;
     }
     return (
-      <ul className="nav">
-        <li className="nav-item" onClick={() => filterItems()}>
-          <a className="nav-link" href="#">
-            همه فست فود ها
-          </a>
-        </li>
-        {categories.map((category) => (
-          <li
-            className="nav-item"
-            key={category.id}
-            onClick={() => filterItems(category.id)}
-          >
+      <div className="ps-3 w-100 d-flex align-items-center justify-content-between gap-5">
+        <ul className="nav">
+          <li className="nav-item" onClick={() => filterItems()}>
             <a className="nav-link" href="#">
-              {category.name}
+              همه فست فود ها
             </a>
           </li>
-        ))}
-      </ul>
+          {categories.map((category) => (
+            <li
+              className="nav-item"
+              key={category.id}
+              onClick={() => filterItems(category.id)}
+            >
+              <a className="nav-link" href="#">
+                {category.name}
+              </a>
+            </li>
+          ))}
+        </ul>
+        {children}
+      </div>
     );
   };
 
